7b83d30fbe0f145bce156ca19e9b33bcac88dd64
Ｆ.Ｙuusuke <>
2019-11-29 19:25:24 +0800

    create.phpを更新
    POSTで送られてきたwordの情報を登録できるようなコードを書いた

--- create.php
+++ create.php
@@ -7,5 +7,12 @@ require_once('Models/Todo.php');
 // ここはindex.phpと繋がってる。詳しくはノートの４ページに書いてあります
 $task = $_POST['task'];
 
+// ９　ここでclassTodoが使えるぜって感じ
+$todo = new Todo();
+// １０　ここは８で送られてきた情報（$taskに代入されているのが８を見ればわかる）
+// を利用してClassTodoのなかにあるcreateメソッドを
+// 実行してもらえるようにコードを書いています
+$todo->create($task);
+
 
 ?>
