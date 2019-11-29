bd2c172218fbc5066fd33908838dc4e73c39f0ed
Ｆ.Ｙuusuke <>
2019-11-29 15:55:06 +0800

    Todo.php更新
    データベースのtasks2を使ってくださいと命令し、さらにそれをtableに代入することによって緊急事態に対応できるようにした。
    dbconnect.phpで作ったclass DbManagerを使いたいから使えるようなるためのコードを書きました。

--- Models/Todo.php
+++ Models/Todo.php
@@ -8,7 +8,15 @@ require_once('config/dbconnect.php');
 // 2class Todoを作る
 class Todo
 {
-    
+    // ３　１行目はデータベースのtasks2を使いたいからそれを使ってねって言っていて
+    // さらにはtasks2をtableに代入することによって他のところでもtableと書くだけで
+    // tasks2を使えるようになるし（もしtasks2じゃなくともっと長い名前だったらめんどい）
+    // 何かあってtasks2じゃなくtasks3になった時は今書いたコードをtasks3に変更するだけで
+    // 全てのファイルに対応できる
+    // ２行目はデータベースの中の値をこのclassの中で使えるように宣言
+    // dbconnect.phpで作ったclass DbManagerを使いたいからここに書いている
+    private $table = 'tasks2';
+    private $db_manager;
 }
 
 ?>
