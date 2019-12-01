ddac8dc0cede470c3667fee0e05308fd6d9ed6a8
Ｆ.Ｙuusuke <>
2019-12-01 11:00:36 +0800

    edit.phpの情報を受け取った
    edit.phpで入力された情報を受け取れるようにして、次はその情報を更新するためにupdeteメソッドを使う

--- update.php
+++ update.php
@@ -4,4 +4,10 @@
 // 35 'Models/Todo.php'ファイルをリクワイヤー(他ファイルを呼ぶための組み込み関数)する
 // なぜならclassTodoの中に使いたいメソッドがあるから
 require_once ('Models/Todo.php');
+
+// ３６　ediat.phpで入力された情報を以下で受け取れるようにしている
+// （var_dump(この中は変数);何か値を取得できているかどうかを確認したい時に使う）
+// $idのなかにPOSTで送られてきたidを格納し、taskの情報も$taskに格納している
+$id =$_POST['id'];
+$task =$_POST['task'];
 ?>
